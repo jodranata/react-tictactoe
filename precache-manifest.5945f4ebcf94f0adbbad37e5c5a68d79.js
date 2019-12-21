2f056c67be405a53b5f95b4b3e21120814236316
Jodi Pranata <jodranata@gmail.com>
2019-12-21 15:02:18 +0700

    Updates

---
+++ precache-manifest.5945f4ebcf94f0adbbad37e5c5a68d79.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "710ae911f30e0182ecad240107d9be4b",
+    "url": "/react-tictactoe/index.html"
+  },
+  {
+    "revision": "f974bb45b6a7852d960e",
+    "url": "/react-tictactoe/static/css/main.d4b158ba.chunk.css"
+  },
+  {
+    "revision": "8a83808158163cc96714",
+    "url": "/react-tictactoe/static/js/2.a7f526f7.chunk.js"
+  },
+  {
+    "revision": "f974bb45b6a7852d960e",
+    "url": "/react-tictactoe/static/js/main.c3683b87.chunk.js"
+  },
+  {
+    "revision": "e1fe0f12dfd12c310d4a",
+    "url": "/react-tictactoe/static/js/runtime-main.b3dc4674.js"
+  }
+]);
